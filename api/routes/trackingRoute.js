b2b55c46f31e46316735697fa60d5ce9cfe25def
cocoalabs-india <salu@cocoalabs.in>
2017-05-30 13:18:29 +0530

    Procfile

--- api/routes/trackingRoute.js
+++ api/routes/trackingRoute.js
@@ -2,6 +2,6 @@
 module.exports = function(app) {
   var tracking = require('../../index');
   //var udpClient = require('../../udp_client.js')
-  //var udpServer = require('../../udp_server.js')
+  var udpServer = require('../../udp_server.js')
   var tcpServer = require('../../tcp_server.js')
 };
